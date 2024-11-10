265c6b58c3cec377752532e88b3f4ee672ab618f
FreeFun <xxfreefunxx@pm.me>
2024-11-10 19:51:00 +0100

    Powershell Lib Check

--- start_cleanup.bat
+++ start_cleanup.bat
@@ -1,3 +1,14 @@
 @echo off
 set "SCRIPT_PATH=%~dp0database_clean_washing.ps1"
-powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%" cleanup
+
+where pwsh >nul 2>nul
+
+if %errorlevel% equ 0 (
+	echo Starting New Powershell
+	pause
+    pwsh -ExecutionPolicy Bypass -File "%SCRIPT_PATH%" cleanup
+) else (
+	echo Starting Old Powershell
+	pause
+    powershell -ExecutionPolicy Bypass -File "%SCRIPT_PATH%" cleanup
+)
